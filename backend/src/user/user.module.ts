f2a33542e14d84334a0a79494c18090cd2ffb7b1
ikurasusshi <kaikai180208@gmail.com>
2025-12-10 17:59:50 +0900

    パッケージのインストールと修正

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { UserService } from './user.service';
 
 @Module({
   imports: [PrismaModule],
